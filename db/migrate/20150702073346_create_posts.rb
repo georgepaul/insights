d70605ee8d2fe3314ebbb458fa9fdf8899a05222
paul c george <georgepaul@live.ca>
2015-07-19 13:51:22 -0400

    migration posts

--- db/migrate/20150702073346_create_posts.rb
+++ db/migrate/20150702073346_create_posts.rb
@@ -7,7 +7,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :post_link
       t.text :post_date
       t.string :post_title
-      t.integer :message_id, :limit => 64.kilobytes + 1
+      t.integer :message_id, :limit => 8
       t.integer :post_number
       t.text :post_body, :limit => 64.kilobytes + 1
 	    t.timestamps
