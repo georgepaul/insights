304857c76168be9c780cda9180bf311a1c469400
paul c george <georgepaul@live.ca>
2015-07-20 17:16:10 -0400

    new migrations

---
+++ app/models/tablerows.rb
@@ -0,0 +1,2 @@
+class Tablerows < ActiveRecord::Base
+end
---
+++ db/migrate/20150720202346_create_tablerows.rb
@@ -0,0 +1,8 @@
+class CreateTablerows < ActiveRecord::Migration
+  def change
+    create_table :tablerows do |t|
+
+      t.timestamps
+    end
+  end
+end
